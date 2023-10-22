084612ab81185baf171611842bef3c588497b37f
JForstman <justlovinglife73@gmail.com>
2023-10-22 08:37:56 -0500

    changes to login & deposit

--- public/home.js
+++ public/home.js
@@ -6,7 +6,7 @@ function Home(){
       header="Welcome to MyBank!"
       title="Click Login above to log into your account"
       text="You can move around using the navigation bar."
-      body={(<img src="bank.png" style={{opacity: "0.2"}} className="img-fluid" alt="Responsive"/>)}
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive"/>)}
     />
   );  
 }
