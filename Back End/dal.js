ca2ca26d7a0040a6867a1027b309917464e26620
JForstman <justlovinglife73@gmail.com>
2023-10-24 15:35:04 -0500

    deletions and insertions

--- Back End/dal.js
+++ Back End/dal.js
@@ -1,35 +1,78 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-let db = null;
-
-//connect to Mongo
+const url         = 'mongodb+srv://justlovinglife73:Sjgv42h48qyFLO9Q@badbank.clghfna.mongodb.net/';
+let db            = null;
+ 
+// connect to mongo
 MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-  console.log('connected to Mongo DB server!');
+    console.log("Connected successfully to db server");
 
-  //connect to myprojet db
-  db = client.db('myproject');
+    // connect to BadBank database
+    db = client.db('BadBank');
 });
 
-//create user account
-function create(name, email, password) {
-  return new Promise((resolve, reject) => {
-    const collection = db.collection('users');
-    const doc = {name, email, password, balance: 0};
-    collection.insertOne(doc, {w:1}, function (err, result) {
-      err ? reject(err) : resolve(doc);
-    });
-  });
-}
-//all users
-function all() {
-  return new Promise((resolve, reject) => {
-    const customers = db
-      .collection('users')
-      .find({})
-      .toArray(function(err, docs) {
-        err ? reject(err) : resolve(docs)
-      });
-  });
+// create user account
+function create(name, email, password){
+    return new Promise((resolve, reject) => {    
+        const collection = db.collection('users');
+        const doc = {name, email, password, balance: 0};
+        collection.insertOne(doc, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });    
+    })
 }
 
-module.exports = {create, all};
+// find user account
+function find(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({email: email})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// find user account
+function findOne(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .findOne({email: email})
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));    
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')            
+            .findOneAndUpdate(
+                {email: email},
+                { $inc: { balance: amount}},
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );            
+
+
+    });    
+}
+
+// all users
+function all(){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+
+module.exports = {create, findOne, find, update, all};
--- Back End/mongo_test.js
+++ Back End/mongo_test.js
@@ -1,29 +1,34 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://justlovinglife73:Sjgv42h48qyFLO9Q@badbank.clghfna.mongodb.net/';
+//const url = 'mongodb://localhost:27017';
+ 
+// connect to mongo
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+  console.log("Connected successfully to server");
 
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
-  console.log('connected')
+    // database Name
+    const dbName = 'BadBank';
+    const db = client.db(dbName);
 
-//database name
-const dbName = 'myproject';
-const db = client.db(dbName);
+    // new user
+    var name = 'user' + Math.floor(Math.random()*10000);
+    var email = name + '@mit.edu';
 
-//new user
-var name = 'user' + Math.floor(Math.random()*10000);
-var email = name + '@mit.edu';
+    // insert into customer table
+    var collection = db.collection('users');
+    var doc = {name, email};
+    collection.insertOne(doc, {w:1}, function(err, result) {
+        console.log('Document insert');
+    });
 
-//insert into customer table
-var collection = db.collection('customers');
-var doc = {name, email};
-collection.insertOne(doc, {w:1}, function (err, result) {
-  console.log('document insert')
-  });
-var customers = db
-  .collection('customers')
-  .find()
-  .toArray(function(err, docs) {
-    console.log('collection:', docs)
-    
-    client.close();
-  });
+    var customers = db
+        .collection('users')
+        .find()
+        .toArray(function(err, docs) {
+            console.log('Collection:',docs);
+
+            // clean up
+            client.close();            
+    });    
+
 });
